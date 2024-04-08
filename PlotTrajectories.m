aa8b15b4e034eaca376f57dc9a9f8f8c5f07c9ca
Kaneda_Norika <nori.op.55151@gmail.com>
2024-04-08 19:20:13 +0900

    utilize FiliInformation.csv

--- PlotTrajectories.m
+++ PlotTrajectories.m
@@ -1,22 +1,16 @@
-% PlotTrajectoryを連続で回すことでTrajectoryを一気に作成するプログラム。
+% PlotTrajectoryを連続で回すことでTrajectoryを一気に作成するプログラム
 
-% 前提：下位のDataフォルダ中に動画あるいはLastFlameファイルと-positionファイルと-infoファイルがある ※WholeInfoは不要
+% 前提：下位のDataフォルダ中に動画あるいはLastFlameファイルと-positionファイルとFileInformation.csvがある
 % やること：撮影番号とシャーレ番号からSampleNameを作成し、エラーでない場合にPlotTrajectoryを実行する
 
-%% 事前準備
-MovieNames = ["20240114_1", "20240116_1", "20240118_1", "20240120_1", "20240209_1", "20240215_1", "20240217_1", "20240222_1", "20240228_1", "20240228_2", "20240228_3", "20240229_1", "20240229_2"];
 dir0 = pwd;
-addpath(fullfile(dir0,"/Data"))
-%% 撮影番号ごとにリストを読み取り、PlotTrajectoryを回す
-for i = 1:length(MovieNames)
-    Info = readtable(fullfile(dir0, "Data", append(MovieNames(i),"-info.csv")), 'VariableNamingRule', 'preserve');
-        Info(Info.Error==1,:)=[];
-    SampleName = append(MovieNames(i), "_", string(Info.SampleNumber));
-    N = height(Info);
-    for j = 1:N
-        PlotTrajectory(SampleName(j))
-    end
-    close all
-end
+addpath(dir0)
 
+%% 情報ファイルを読み込む
+
+Info = readtable(fullfile(dir0, "Data", "FileInformation.csv"));
+
+%% 各撮影ファイルでPlotTrajectoryを実行
+
+arrayfun(@PlotTrajectory, Info.FileName)
 
